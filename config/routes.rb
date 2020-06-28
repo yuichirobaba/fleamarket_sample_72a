2ac6e70ebe9eed1463281beefa55eb28202640b2
yuichirobaba <63151680+yuichirobaba@users.noreply.github.com>
2020-06-28 16:52:45 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
   
-  devise_for :users
    
   
   root 'items#index'
 
-  resources :signup, only: [:create] do 
-    collection do
-      get 'new'
-      get 'user_registration'
-    end
-end
+
 end
