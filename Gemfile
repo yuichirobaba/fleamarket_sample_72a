0c79414adba00cf5ab217c82b4f9bf38b31407d6
naoya-f <naoya0867@gmail.com>
2020-07-12 16:50:24 +0900

    mypage commit conflict

--- Gemfile
+++ Gemfile
@@ -70,5 +70,3 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 group :production do
   gem 'unicorn'
 end
-
-gem 'haml-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  get 'markup_preview', to: 'markup_preview#index'
-  get 'users', to: 'users#index'
   root 'items#index'
 end
