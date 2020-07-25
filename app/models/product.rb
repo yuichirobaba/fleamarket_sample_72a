fa24000258598cab93646f79a8c4afdfe41e098b
yukireonsousi <yukireonsousi@gmail.com>
2020-07-25 23:23:05 +0900

    uu

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :user, dependent: :destroy
-  # belongs_to :category, dependent: :destroy
+  belongs_to :category, dependent: :destroy
   # has_many :comments, dependent: :destroy
   # has_many ;images, dependent: :destroy
 end
--- db/migrate/20200703130752_create_products.rb
+++ db/migrate/20200703130752_create_products.rb
@@ -1,7 +1,6 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-      t.string :name, null: false
       t.text :comment, null: false
       t.integer :price, null: false
       t.string :brand
@@ -10,7 +9,7 @@ class CreateProducts < ActiveRecord::Migration[5.2]
       t.string :area, null: false
       t.string :days, null: false
       t.references :user, foreign_key: true
-      # t.reference :category, foreign_key: true
+      t.references :category, foreign_key: true
       t.timestamps
     end
   end
