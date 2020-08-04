d95f013697db300dbd626e3783d8043d81c24d3c
yukireonsousi <yukireonsousi@gmail.com>
2020-08-04 17:46:17 +0900

    master

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,6 +21,39 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit  #newとの違いはまず検索をかけなくてはいけない点にある。よってfind、find_by,whereなどの検索メソッドが必須
+    @product = Product.find(params[:id])
+    @user = User.find(@product.user_id)#最初にこの処理を持ってくる事で、商品を出品したユーザーのみが編集出来る
+    @images = Image.where(product_id: params[:id])
+    @images_first = Image.where(product_id: params[:id]).first #firstは配列の先頭の要素を持ってくる、恐らくトップページ等に映し出される商品の顔(仮)
+
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if product_params[:images_attributes].nil?
+      flash.now[:alert] = '更新できませんでした 【画像を１枚以上入れてください】' #最初に記述していればelsef処理を書かなくていい
+      render :edit
+    else
+      exit_ids = []
+      product_params[:images_attributes].each do |a,b|
+        exit_ids << product_params[:images_attributes].dig(:"#{a}",:id).to_i #digメソッドによってproductが:images_attributesによってネストしている画像データ（to_iメソッドで数字が割り振られてる）が一部、空だったとしてもエラーが出ず処理される
+      end
+      ids = Image.where(product_id: params[:id]).map{|image| image.id } # mapメソッドで配列をそのあとの変数imageでid事に処理
+      delete__db = ids - exit_ids#もとからあった画像データ - 更新された画像データ  (id付き)
+      Image.where(id:delete__db).destroy_all #元あった画像データは削除
+      @product.touch #touchメソッド、更新日時を現在時刻で更新
+      if @product.update(product_params)
+
+        redirect_to  products_details_path #詳細ページにいく様にしてみた
+      else
+        flash.now[:alert] = '更新できませんでした'
+        render :edit
+      end
+    end
+  
+
   private
 
   def product_params
@@ -28,4 +61,5 @@ class ProductsController < ApplicationController
       :name, :comment, :price, :brand, :size, :shippingcharge, :status,
       :area, :days, :category_id, images_attributes: [:image])
   end
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :delivary_data
+  # devise_for :delivary_data
   devise_for :create_users,controllers:{
     registrations: 'registrations',
   }
   root 'items#index'
-  resources :products, except: :show
+  resources :products, except: :show #商品データ、下記に商品詳細ページ用のルーティングがあるので(showアクションを除外)
   resources :create_users
   resources :signup do
     collection do
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
       get 'new2'
       get 'new3'
       get 'done'
-  resources :products_details
+  resources :products_details #商品詳細ページ
  end
 end
 end
