58149a2f9b7b53d3aa4455a1ab109e351c28164e
yuichirobaba <63151680+yuichirobaba@users.noreply.github.com>
2020-08-08 18:50:29 +0900

    Update carierwave.rb

--- config/initializers/carierwave.rb
+++ config/initializers/carierwave.rb
@@ -8,6 +8,7 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: Rails.application.credentials.aws_access_key_id,
+    aws_secret_access_key: Rails.application.credentials.aws_secret_access_key,
     region: 'ap-northeast-1'
   }
 
