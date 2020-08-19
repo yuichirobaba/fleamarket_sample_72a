2e66a45868c85b49c03161f32ab1a5a8b319a170
seiko mori <latteconnect2020@gmail.com>
2020-08-19 21:42:45 +0900

    Revert "commit"
    This reverts commit f8a07ebc7c98c2ffa02f0d48a93a56498a9537c1.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_10_122852) do
+ActiveRecord::Schema.define(version: 2020_08_13_061116) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -36,8 +36,8 @@ ActiveRecord::Schema.define(version: 2020_08_10_122852) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_create_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_create_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "delivary_data", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
