50e45b356cd528c8afa32465b0a810a4af5aabe1
seiko mori <latteconnect2020@gmail.com>
2020-08-26 21:11:54 +0900

    commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+require "csv"
+
+CSV.foreach('db/category.csv') do |row|
+  Category.create(:id => row[0], :name => row[1], :ancestry => row[2])
+end 
+  
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
