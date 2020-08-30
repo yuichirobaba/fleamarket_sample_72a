38daabf6a3833293d15f95e876140766d9d3928e
yuichirobaba <63151680+yuichirobaba@users.noreply.github.com>
2020-08-30 15:03:52 +0900

    Update products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -36,7 +36,7 @@ class ProductsController < ApplicationController
 
   def show
     @category_id = @product.category_id
-    @category_parent = Category.find(@category_id).parent
+    @category_parent = Category.find(@category_id).parent.parent
     @category_child = Category.find(@category_id).parent
     @category_grandchild = Category.find(@category_id)
   end
